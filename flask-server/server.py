b205adadca8d4e568fb898f1d7a5f3f364420d50
Brayan Pichardo <brayanpichardo8@gmail.com>
2023-11-29 21:22:52 -0500

    simple Iphone setup & basic file structure

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,28 @@
+import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+API_KEY = "AIzaSyDRN4nkBauq2pSLMdDSFhx2z8QAsCRtU7M"
+BASE_URL =  "https://maps.googleapis.com/maps/api/place"
+
+@app.route('/GetCompanyByType', methods=['GET'])
+def GetCompanyByType():
+    endpoint = f"{BASE_URL}/nearbysearch/json"
+    params = {
+        'keyword': 'cruise',
+        'location': "40.712776,-74.005974",
+        'radius': '1500',
+        'type': 'restaurant',
+        'key': API_KEY,
+    }
+    response = requests.get(endpoint, params=params)
+    data = response.json()
+
+    return data
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
