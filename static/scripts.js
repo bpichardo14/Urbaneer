cc4dba1f83801c5f2e9f92be6d03dc0eb28434c3
Da Hoon Oh <yeodh195@gmail.com>
2023-12-01 18:10:13 -0500

    Update profile_like page

--- static/scripts.js
+++ static/scripts.js
@@ -1,44 +1,52 @@
 window.onload = function() {
-    const placesList = document.getElementById('places-list');
-    fetch('/api/places')
-      .then(response => response.json())
-      .then(data => {
-        data.forEach(place => {
-          const listItem = document.createElement('li');
-          listItem.className = 'place-card';
-          
-          const image = document.createElement('img');
-          image.className = 'place-image';
-          image.src = "/static/image/" + place.image;
-          image.alt = place.name;
-          image.onclick = function() {
-            window.location.href = '/place/' + place.name;
-          };
-  
-          const infoDiv = document.createElement('div');
-          infoDiv.className = 'place-info';
-          infoDiv.innerHTML = `
-            <h3>${place.name}</h3>
-            <p>${place.distance}</p>
-            <p>Liked by ${place.likes} others</p>
-          `;
-  
-          listItem.appendChild(image);
-          listItem.appendChild(infoDiv);
-          placesList.appendChild(listItem);
-        });
-      })
-      .catch(error => console.error('Error fetching data:', error));
+  // Common function to load places
+  function loadPlaces(placesListId) {
+      const placesList = document.getElementById(placesListId);
+      if (!placesList) return; // Exit if the element doesn't exist
 
-    function updateTime() {
-        const now = new Date();
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        document.getElementById('current-time').textContent = hours + ':' + minutes;
-    }
+      fetch('/api/places')
+        .then(response => response.json())
+        .then(data => {
+          data.forEach(place => {
+            const listItem = document.createElement('li');
+            listItem.className = 'place-card';
+            
+            const image = document.createElement('img');
+            image.className = 'place-image';
+            image.src = "/static/image/" + place.image;
+            image.alt = place.name;
+            image.onclick = function() {
+              window.location.href = '/place/' + place.name;
+            };
+    
+            const infoDiv = document.createElement('div');
+            infoDiv.className = 'place-info';
+            infoDiv.innerHTML = `
+              <h3>${place.name}</h3>
+              <p>${place.distance}</p>
+              <p>Liked by ${place.likes} others</p>
+            `;
+    
+            listItem.appendChild(image);
+            listItem.appendChild(infoDiv);
+            placesList.appendChild(listItem);
+          });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+  }
 
-    setInterval(updateTime, 60000);
-    updateTime();
+  // Load places on index page
+  loadPlaces('places-list');
+  loadPlaces('profile-places-list');
 
-  };
-  
+  // Existing function to update time
+  function updateTime() {
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      document.getElementById('current-time').textContent = hours + ':' + minutes;
+  }
+
+  setInterval(updateTime, 60000);
+  updateTime();
+};
