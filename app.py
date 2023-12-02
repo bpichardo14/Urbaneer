7fa98b947ced0b065f9bcc61c7f32e1befcb20c0
KeunWoo Song <ks3651@columbia.edu>
2023-12-01 20:46:15 -0500

    update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, jsonify
-from fake_data import fake_places
+from fake_data import fake_places, friends_places
+from fake_friends import fake_friends
+from fake_activity import fake_activity
+
 
 app = Flask(__name__)
 
@@ -11,6 +14,14 @@ def get_places():
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/activity')
+def activity():
+    return render_template('activity.html', activities=fake_activity)
+
+@app.route('/friends')
+def friends():
+    return render_template('friends.html', friends=fake_friends)
 
 @app.route('/profile')
 def profile():
