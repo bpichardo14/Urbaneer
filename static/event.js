361f18735029894752d27011c332ebf5ce5d9e7e
KeunWoo Song <ks3651@columbia.edu>
2023-12-17 03:59:33 -0500

    edit event

---
+++ static/event.js
@@ -0,0 +1,28 @@
+function openTextScreen(contactName) {
+  window.location.href = `/text_screen?contact=${contactName}`;
+}
+
+function toggleSortOptions() {
+  document.getElementById("sortOptions").classList.toggle("show");
+}
+
+function toggleFilterOptions() {
+  document.getElementById("filterOptions").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".sort-icon, .filter-icon")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function redirectToEventDetails(eventName) {
+  // Replace this URL with the actual URL pattern for your event details page
+  window.location.href = `/event_details/${eventName}`;
+}
